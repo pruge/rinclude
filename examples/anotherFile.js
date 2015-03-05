ae0ad174e5993f91dc4a558b6fecc603fdad6254
prugel <strive1024@gmail.com>
2015-03-05 16:42:36 +0900

    edit example

---
+++ examples/anotherFile.js
@@ -0,0 +1,26 @@
+var include = require('../');
+
+include('display').print();
+include('show').print();
+
+var timer = include('timer');
+timer.start.print();
+timer.pause.print();
+timer.stop.print();
+
+console.log();
+
+include('app.display').print();
+include('app.show').print();
+
+var appTimer = include('app.timer');
+appTimer.start.print();
+appTimer.pause.print();
+appTimer.stop.print();
+
+console.log();
+
+var timer2 = include('timer2');
+
+timer2.start.print();
+timer2.util.stop.print();
--- examples/app.js
+++ examples/app.js
@@ -3,27 +3,5 @@ var include = require('../');
 include.path('./lib');
 include.path('./app', 'app');
 
-include('display').print();
-include('show').print();
 
-var timer = include('timer');
-timer.start.print();
-timer.pause.print();
-timer.stop.print();
-
-console.log();
-
-include('app.display').print();
-include('app.show').print();
-
-var appTimer = include('app.timer');
-appTimer.start.print();
-appTimer.pause.print();
-appTimer.stop.print();
-
-console.log();
-
-var timer2 = include('timer2');
-
-timer2.start.print();
-timer2.util.stop.print();
+require('./anotherFile');
